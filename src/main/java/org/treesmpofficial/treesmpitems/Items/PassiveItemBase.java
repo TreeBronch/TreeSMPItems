d1f3cff91144ef7c2da40913fb2055f2e6d03bbe
TreeBronch <74438588+TreeBronch@users.noreply.github.com>
2024-02-11 20:29:51 -0500

    Init

---
+++ src/main/java/org/treesmpofficial/treesmpitems/Items/PassiveItemBase.java
@@ -0,0 +1,62 @@
+package org.treesmpofficial.treesmpitems.Items;
+
+import com.google.common.collect.Multimap;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class PassiveItemBase {
+    private String name; // Name of the item
+    private String description;
+
+    public double damagePercentIncrease = 0;
+    public double healthPercentIncrease = 0;
+
+    private int maxStack;
+    private int rarity;
+    private Material mat = Material.DIAMOND;
+
+    public PassiveItemBase(String name, String description, int maxStack, int rarity, int id) {
+        this.name = name;
+        this.description = description;
+        this.maxStack = maxStack;
+        this.rarity = rarity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getMaxStack() {
+        return maxStack;
+    }
+
+    public int getRarity() {
+        return rarity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ItemStack buildItem(){
+        ItemStack item = new ItemStack(mat);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        meta.setLore(Arrays.asList(description));
+        meta.setAttributeModifiers((Multimap<Attribute, AttributeModifier>) Arrays.asList(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier("generic.attack_damage", damagePercentIncrease, AttributeModifier.Operation.MULTIPLY_SCALAR_1)));
+        meta.setAttributeModifiers((Multimap<Attribute, AttributeModifier>) Arrays.asList(Attribute.GENERIC_MAX_HEALTH, new AttributeModifier("generic.max_health", healthPercentIncrease, AttributeModifier.Operation.MULTIPLY_SCALAR_1)));
+        item.setItemMeta(meta);
+        return item;
+    }
+}
---
+++ src/main/java/org/treesmpofficial/treesmpitems/Utils/Rarities.java
@@ -0,0 +1,10 @@
+package org.treesmpofficial.treesmpitems.Utils;
+
+public class Rarities {
+    public static final int COMMON = 0;
+    public static final int UNCOMMON = 1;
+    public static final int RARE = 2;
+    public static final int EPIC = 3;
+    public static final int LEGENDARY = 4;
+    public static final int MYTHIC = 5;
+}
